1b5c2093f94fbf65208cb1ecaca502e80237e5c4
DalenMax <mxt.tung@gmail.com>
2025-03-02 13:36:32 +0700

    tact

---
+++ src/components/coming-soon.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ComingSoon = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-black">
+      <div className="text-center p-12 mt-16">
+        <h1 className="text-8xl font-bold text-yellow-400 mb-4">Coming Soon</h1>
+        <div className="w-32 h-2 bg-yellow-400 mx-auto my-6"></div>
+      </div>
+    </div>
+  );
+};
+
+export default ComingSoon;
